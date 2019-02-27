a7b4d78a510a5b89698d273ecb0fa1994a239a7e
Logicaldownfall <46576995+Logicaldownfall@users.noreply.github.com>
2019-02-27 14:22:16 -0800

    Sound manager (didn't track components correctly before)

---
+++ Assets/src/Jaidin/SoundManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+    private AudioSource musicSource;
+    private AudioClip song;
+    public float TimeElapsed = 0;
+    public float SongDuration;
+    void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        song = musicSource.clip;
+        //Add in other audio sources here
+        //Also need the ability to choose music through menu (currently just has one song)
+        SongDuration = song.length;
+    }
+
+    public float[] AnalyzeSong(AudioSource song)
+    {
+        float[] samples = song.GetOutputData(1024, 1);
+        //more analysis done here
+        return samples;
+    }
+    public void PauseMusic()
+    {
+        musicSource.Pause();
+        //pause time here
+    }
+    public void UnPauseMusic()
+    {
+        musicSource.UnPause();
+        //unpause time in here also
+    }
+    private void BeginMusic()
+    {
+        musicSource.Play(0);
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TimeElapsed += Time.deltaTime;
+        //Add in time display here later
+        if(TimeElapsed >= SongDuration) //Right now, song will loop endlessly
+        {
+            TimeElapsed = 0;
+            BeginMusic();
+        }
+    }
+}
