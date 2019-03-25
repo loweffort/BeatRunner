1138d72932fbc34999c36aed26e9b53bdac72d65
Lennin Rodriguez <38442487+lennin14@users.noreply.github.com>
2019-03-25 15:13:05 -0700

    Testing
    Making sure I can push

---
+++ Assets/src/Lennin/Lennin Test Script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerObstacleTest : MonoBehaviour
+{
+    public SoundManager soundManager;
+    public HUD hud;
+    public BarObstacle barObstacle;
+    public PlayerCharacter playerCharacter;
+    int prevTime;
+    int speedIncrement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevTime = (int)Time.time;
+        barObstacle.name = "barObstacle";
+    }
+
+    private void RestartGame()
+     {
+        barObstacle.transform.position = new Vector3(0, 6, 850);
+        barObstacle.transform.rotation = new Quaternion (0, 0, 0, 1);
+        playerCharacter.transform.position = new Vector3(0, 6, 4);
+        playerCharacter.transform.rotation = new Quaternion(0, 0, 0, 1);
+        playerCharacter.SendMessage("ResetVelocity", 0.5f, SendMessageOptions.RequireReceiver);
+        hud.SendMessage("ResetScore", 0.5f, SendMessageOptions.RequireReceiver);
+        soundManager.StopMusic();
+        soundManager.BeginMusic();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        int newTime = (int)Time.time;
+        if (prevTime < newTime)
+        {
+            speedIncrement++;
+            prevTime = newTime;
+        }
+        barObstacle.setVelocity(0 - 100 * speedIncrement);
+        if (barObstacle.transform.position.z < 3)
+        {
+            barObstacle.transform.position = new Vector3(0, 6, 850);
+        }
+   }
+}
