5203e85632c0cf9e289979e3f9f8fbbba2028484
Jcraw07 <47007566+Jcraw07@users.noreply.github.com>
2019-04-13 23:38:22 -0700

    Pushing Jump Back
    Pushing Jump Back

---
+++ Assets/src/Yiqing/character_jump_1.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_jump_1 : MonoBehaviour
+{
+    //private boolen
+    bool initJump = false;
+    //Transform player;
+
+    // Start is called before the first frame update
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y < 6) // input jump and the character height < 5.6
+        {
+            initJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (initJump)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 50, 0);//jump height
+            initJump = false;
+        }
+    }
+
+}
