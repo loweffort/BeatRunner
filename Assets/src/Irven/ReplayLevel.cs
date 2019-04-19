430e063c932a20c262cc731df8af8b5f8acf0357
Irv <perez.irven@gmail.com>
2019-04-19 15:43:17 -0700

    Updated the ending Menu

---
+++ Assets/src/Irven/ReplayLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayLevel : MonoBehaviour
+{
+    public void startGame()
+    {
+        Debug.Log("starting");
+        SceneManager.LoadScene(sceneName: "DifficultySelection");
+    }
+}
