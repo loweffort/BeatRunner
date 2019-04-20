225bdaac4072a8a93ca12a173a60b5d101d025cb
Irv <perez.irven@gmail.com>
2019-04-20 02:34:29 -0700

    Added a singleton

---
+++ Assets/src/Irven/SingletonClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SingletonClass : MonoBehaviour
+{
+
+    public static SingletonClass instance;
+    public string currentClass = "CS-395";
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void addClassInformation(string updatedName)
+    {
+        currentClass = updatedName;
+    
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(870, 20, 300, 300), "Class: " + currentClass);
+    }
+
+}
---
+++ Assets/src/Irven/singletonCall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class singletonCall : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+        string updateClassName = "Analysis of Algorithms";
+        SingletonClass.instance.addClassInformation(updateClassName);
+    }
+
+
+}
